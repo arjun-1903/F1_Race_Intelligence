0ea178873a2bb208ee857c43b361cabd69ee4fbd
Arjun Rajesh <rit2arjun@gmail.com>
2026-04-17 23:40:09 +0530

    chore: freeze requirements and configure vite API url for cloud deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = import.meta.env.VITE_API_BASE || "http://localhost:8000";
 
 export const getRaces = (year) =>
   axios.get(`${BASE_URL}/races?year=${year}`);
